003eb2f30737b55ee5a3a16af5150409a11f40a9
Rayan-Wong <134691628+Rayan-Wong@users.noreply.github.com>
2026-08-21 13:33:42 +0000

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,17 @@
+import pluginVue from "eslint-plugin-vue";
+import typescriptEslint from "typescript-eslint";
+
 export default [
     {
         ignores: ["**/*.json"]
     },
+    ...pluginVue.configs["flat/recommended"],
+    {
+        files: ["**/*.vue"],
+        languageOptions: {
+            parserOptions: {
+                parser: typescriptEslint.parser
+            }
+        }
+    }
 ];
